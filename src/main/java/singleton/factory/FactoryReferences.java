b797dfe60f91c3da802fc7812e6d3a340915371a
Sonia Martínez Martín <inmakia@gmail.com>
2014-10-20 00:47:29 +0200

    Añadido ejercicio singleton.factory

---
+++ src/main/java/singleton/factory/FactoryReferences.java
@@ -0,0 +1,39 @@
+package singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FactoryReferences {
+    private Map<String, Integer> references;
+
+    private int reference;
+    
+    private static FactoryReferences factory;
+
+    private FactoryReferences() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+    
+    public static FactoryReferences getFactory(){
+        if (factory == null) {
+            factory = new FactoryReferences();
+        }
+        return factory;
+    }
+
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
